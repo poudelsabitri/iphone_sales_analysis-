1d426d5418543628e407992ca66582e4bd1b5423
Sabitri Poudel || Computer Engineer Graduate <32294108+poudelsabitri@users.noreply.github.com>
2024-10-21 13:42:31 -0700

    Create product_data_collector_api.py

---
+++ product_data_collector_api.py
@@ -0,0 +1,18 @@
+from pyspark.sql import SparkSession
+
+# Initialize Spark session
+spark = SparkSession.builder.appName("Product data collector").enableHiveSupport().getOrCreate()
+
+
+def product_data_collector_api(spark, parquet_file_path):
+    # Load the product data from the parquet file
+    product_df = spark.read.parquet(parquet_file_path)
+
+    # Save the data to a non-partitioned Hive table
+    product_df.write.mode("overwrite").saveAsTable("iphone_sales_analysis.product_hive_table")
+
+    return "iphone_sales_analysis.product_hive_table"
+
+if __name__ == '__main__':
+    filepath = 'file:///home/takeo/iphone_sales_analysis_project/sales_Table'
+    sales_data_collector_api(spark, filepath)
